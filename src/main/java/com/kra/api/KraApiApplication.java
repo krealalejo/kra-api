c9a1dc8963e8331bf592e1c46edbefd6fd0b6b0b
Kevin Real Alejo <krealalejo@gmail.com>
2026-04-06 13:52:28 +0200

    chore: initialize Spring Boot 3.5 + Maven + DDD package scaffold

---
+++ src/main/java/com/kra/api/KraApiApplication.java
@@ -0,0 +1,13 @@
+package com.kra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kra/api/KraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kra.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
